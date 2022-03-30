c5452ebb1f28f0019aec56d6cbd6d472a4022e7f
Jennifer Torres <xtormin.jtf@gmail.com>
2022-03-30 02:30:46 +0200

    Update database path

--- setup/server.sh
+++ setup/server.sh
@@ -1,2 +1,2 @@
-docker run -p 5432:5432 --name formshunterdb -v /Users/j4ckmln/Xtormin/Development/Dev/formshunter/database:/var/lib/postgresql/data -e POSTGRES_PASSWORD=secret -d postgres
+docker run -p 5432:5432 --name infranalyzerdb -v $(pwd)/database:/var/lib/postgresql/data -e POSTGRES_PASSWORD=secret -d postgres
 docker start formshunterdb
