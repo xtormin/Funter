45aa4eedd6e7e6a25e4dd83c02516e73ca2eaf4a
Xtormin <jtorresf42@gmail.com>
2022-09-26 17:25:47 +0200

    Add setup settings

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,5 @@
-apt update
-apt upgrade
-apt install docker.io docker-compose python3 pip3
+sudo apt update
+sudo apt upgrade
+sudo apt install docker.io docker-compose python3 pip3
 mkdir database outputs api app-ui
 chmod +x start.sh
-pip3 install -r requirements.txt
