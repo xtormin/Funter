899e73ea94941d16b7e5fab9b69b69874bc1c4d3
Xtormin <xtormin@protonmail.com>
2023-06-30 20:06:33 +0200

    Fix package name

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,5 +1,5 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install docker.io docker-compose python3 pip3
+sudo apt install docker.io docker-compose python3 python3-pip
 mkdir database outputs api app-ui
 chmod +x server.sh
