94e8de0d4d0f13f3524cefad4aa7a4674bf2f307
Xtormin <xtormin@protonmail.com>
2023-06-30 20:08:34 +0200

    Improve classes

--- backend/db/models.py
+++ backend/db/models.py
@@ -3,68 +3,96 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 from mongoengine import connect, disconnect, DynamicDocument, StringField, ReferenceField
 from pathlib import Path
-from backend.db.MongoAPI import *
 from dotenv import load_dotenv
-
-try:
-    mongoengine = MongoAPI()
-    client = mongoengine.client
-    db = mongoengine.db
-    conn = mongoengine.conn
-
-except Exception as e:
-    print('|-| [ERROR] Error connecting to database...')
-    print(e)
 
 # DATABASE MODELS
 
 class Project(DynamicDocument):
-    __tablename__ = 'project'
-    name = StringField(required=True, max_length=200, unique=True)
+    """
+    This class represents a project document in the database.
+    Each project has a unique name.
+    """
+
+    meta = {'collection': 'project',  # Explicitly specify collection name
+            'indexes': ['name']}  # Adding an index on the 'name' field
+
+    name = StringField(
+        required=True,
+        max_length=200,
+        unique=True,
+        help_text="The unique name of the project"
+    )
+
 
 class Scope(DynamicDocument):
-    __tablename__ = 'scope'
-    name = StringField(required=True, max_length=200, unique=True)
-    id_project = ReferenceField(Project)
+    """
+    This class represents a scope document in the database.
+    Each scope has a unique name and is associated with a project.
+    """
+    meta = {'collection': 'scope',  # Explicitly specify collection name
+            'indexes': ['name']}  # Adding an index on the 'name' field
 
-# class Form(DynamicDocument):
-#     __tablename__ = 'form'
-#     url_name = StringField(max_length=200, default='None')
-#     action = StringField(max_length=200, default='None')
-#     method = StringField(max_length=200, default='None')
-#     input_name = StringField(max_length=200, default='None')
-#     type = StringField(max_length=200, default='None')
-#     value = StringField(max_length=500, default='None')
-#     options = StringField(max_length=200, default='None')
-#     id_scope = ReferenceField(Scope)
+    name = StringField(
+        required=True,
+        max_length=200,
+        unique=True,
+        help_text="The unique name of the scope"
+    )
+
+    project_reference = ReferenceField(
+        Project,
+        required=True,
+        help_text="Reference to the associated project"
+    )
 
 class Form(DynamicDocument):
-    __tablename__ = 'form'
-    url_name = StringField(max_length=200, default='None')
-    form_name = StringField(max_length=200, default='None')
-    action = StringField(max_length=200, default='None')
-    method = StringField(max_length=200, default='None')
-    input_name = StringField(max_length=200, default='None')
-    type = StringField(max_length=200, default='None')
-    value = StringField(max_length=500, default='None')
-    options = StringField(max_length=200, default='None')
+    """
+    This class represents a form document in the database.
+    It stores information about a form, including its URL, name,
+    action, method, input_name, type, value, and options.
+    """
+    meta = {
+        'collection': 'form',  # Explicitly specify collection name
+        'indexes': ['url_name', 'form_name']  # Example of adding indexes
+    }
 
-# WEB OUTPUT MODELS
-class Data(object):
-    def __init__(self, url_name, action, method, input_name, type, value):
-        self.url_name = str(url_name)
-        self.action = str(action)
-        self.method = str(method)
-        self.input_name = str(input_name)
-        self.type = str(type)
-        self.value = str(value)
+    url_name = StringField(
+        max_length=200,
+        help_text="The URL name of the form"
+    )
 
-    def forms_json(self):
-        return {
-            "url_name": self.url_name, 
-            "action": self.action, 
-            "method": self.method,
-            "input_name": self.input_name,
-            "type": self.type,
-            "value": self.value
-        }
+    form_name = StringField(
+        max_length=200,
+        help_text="The name of the form"
+    )
+
+    action = StringField(
+        max_length=200,
+        help_text="The action attribute of the form"
+    )
+
+    method = StringField(
+        max_length=200,
+        choices=['GET', 'POST'],  # Example of adding validation for the method field
+        help_text="The method of the form (e.g., GET, POST)"
+    )
+
+    input_name = StringField(
+        max_length=200,
+        help_text="The input name of the form"
+    )
+
+    input_type = StringField(  # Renamed 'type' to 'input_type' for clarity
+        max_length=200,
+        help_text="The input type of the form"
+    )
+
+    value = StringField(
+        max_length=500,
+        help_text="The value attribute of the form's input"
+    )
+
+    options = StringField(
+        max_length=200,
+        help_text="The options for the form's input"
+    )
